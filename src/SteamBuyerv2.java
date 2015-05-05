c43ec23fee87522562cbacc4041da1f1a36ae468
Pablo Mendoza <pablo.mendoza@openenglish.com>
2015-05-05 14:16:34 -0300

    Added buy validation and fixes

--- src/SteamBuyerv2.java
+++ src/SteamBuyerv2.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -13,108 +14,105 @@ import org.openqa.selenium.firefox.internal.ProfilesIni;
 import com.google.common.collect.Lists;
 
 public class SteamBuyerv2 {
-	
+
 	@Test
-	public void steamBuyerTest() throws Exception{
-		
+	public void steamBuyerTest() throws Exception {
+
 		ProfilesIni profile = new ProfilesIni();
 		FirefoxProfile ffprofile = profile.getProfile("default");
 		WebDriver driver = new FirefoxDriver(ffprofile);
-		
-		
+
 		WebElement price = null;
 		List<WebElement> priceslist;
-		
-		
+
 		String priceString;
 		String itemname;
-		
-		Item tec9 =  new Item("https://steamcommunity.com/market/listings/730/StatTrak%E2%84%A2%20Tec-9%20%7C%20Isaac%20%28Field-Tested%29", 1.00, 1);
-		Item tec10 =  new Item("https://steamcommunity.com/market/listings/730/StatTrak%E2%84%A2%20Tec-9%20%7C%20Isaac%20%28Field-Tested%29", 1.00, 1);
-		
-		List<Item> items  = Lists.newArrayList(tec9, tec10);
-		
-		
-//		String[] pageURL = {"https://steamcommunity.com/market/listings/730/M4A1-S%20%7C%20Dark%20Water%20%28Minimal%20Wear%29",
-//							"https://steamcommunity.com/market/listings/730/StatTrak%E2%84%A2%20USP-S%20%7C%20Guardian%20%28Field-Tested%29",
-//							"https://steamcommunity.com/market/listings/730/StatTrak%E2%84%A2%20Tec-9%20%7C%20Isaac%20%28Field-Tested%29",
-//							"https://steamcommunity.com/market/listings/730/StatTrak%E2%84%A2%20P250%20%7C%20Supernova%20%28Minimal%20Wear%29",
-//							,	
-//							};
-		
-		
-//		int quantity[] = new int[] {0,0,4,0,0};
-//		Double budgetPrice[] = new Double[] {3.25,2.85,1.00,2.40,1.35};
-		
-		
+
+		List<Item> items = Lists.newArrayList();
+		items.add(new Item("https://steamcommunity.com/market/listings/730/M4A1-S%20%7C%20Dark%20Water%20%28Minimal%20Wear%29", 2.5, 1));
+		items.add(new Item("https://steamcommunity.com/market/listings/730/StatTrak%E2%84%A2%20Tec-9%20%7C%20Isaac%20%28Field-Tested%29", 0.85, 1));
+		items.add(new Item("https://steamcommunity.com/market/listings/730/StatTrak%E2%84%A2%20USP-S%20%7C%20Guardian%20%28Field-Tested%29", 2.00, 1));
+
 		boolean flag = true;
 		int listpos = 0;
-		
+
 		Double trimmedPrice;
-		
+
 		Calendar cal;
 		SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
-		
+
 		driver.get("https://steamcommunity.com/login/");
 		driver.findElement(By.id("steamAccountName")).sendKeys("mendion82");
 		driver.findElement(By.id("steamPassword")).sendKeys("paranoia290");
 		driver.findElement(By.id("SteamLogin")).click();
-		
+
 		Thread.sleep(5000);
 
-		while(true){
-			
-			for (Item item : items) {
-				
-				if(item.getQuantity() > 0){
-					
-					flag = true;
-					
-					while(flag){
-					
-						driver.get(item.getPageURL());
-					
-						priceslist = driver.findElements(By.className("market_listing_price"));
-					
-						driver.findElement(By.className("market_listing_price")).click();				
-					
-						price = priceslist.get(listpos);
-						priceString = price.getText();
-									
-					
-						if(!"Sold!".equals(priceString)){
-						
-							trimmedPrice = Double.valueOf(priceString.replaceAll( "[^0-9.]", "" ));
-							cal = Calendar.getInstance();
-							itemname = driver.findElement(By.xpath("//*[@id='mainContents']/div[1]/div/a[2]")).getText();
-							System.out.println( (items.indexOf(item))+1 + " - " + itemname + " - " + sdf.format(cal.getTime()) + ": " + trimmedPrice);
-							listpos = 0;
-							flag = false;
-							if(trimmedPrice <= item.getBudgetPrice()){
-							
-								
-								driver.findElement(By.className("item_market_action_button")).click();
-								driver.findElement(By.id("market_buynow_dialog_accept_ssa")).click();
-								driver.findElement(By.xpath("//*[@id='market_buynow_dialog_purchase']/span")).click();
-								item.substractItemQuantity();
-								Thread.sleep(8000);
-								}
-							
-							}else{
-							
-								listpos += 2;
-							
-						}
-					
-					}
-					
-				}						
-				
-			}
-			
-		}
-		
-	}
-	
-}
+		while (true) {
 
+			for (Item item : items) {
+
+				if (item.getQuantity() > 0) {
+
+					flag = true;
+
+					while (flag) {
+
+						driver.get(item.getPageURL());
+
+						try {
+
+							priceslist = driver.findElements(By.className("market_listing_price"));
+
+							driver.findElement(By.className("market_listing_price")).click();
+
+							price = priceslist.get(listpos);
+							priceString = price.getText();
+
+							if (!"Sold!".equals(priceString)) {
+
+								trimmedPrice = Double.valueOf(priceString.replaceAll("[^0-9.]", ""));
+								cal = Calendar.getInstance();
+								itemname = driver.findElement(By.xpath("//*[@id='mainContents']/div[1]/div/a[2]")).getText();
+								System.out.println((items.indexOf(item)) + 1 + " - " + itemname + " - " + sdf.format(cal.getTime()) + ": " + trimmedPrice);
+								listpos = 0;
+								flag = false;
+								if (trimmedPrice <= item.getBudgetPrice()) {
+
+									driver.findElement(By.className("item_market_action_button")).click();
+									driver.findElement(By.id("market_buynow_dialog_accept_ssa")).click();
+									driver.findElement(By.xpath("//*[@id='market_buynow_dialog_purchase']/span")).click();
+									Thread.sleep(8000);
+
+									if ("overflow: visible;".equals(driver.findElement(By.id("market_buynow_dialog_purchasecomplete_message")).getAttribute("style"))) {
+										item.substractItemQuantity();
+									} else {
+										System.out.println(driver.findElement(By.id("market_buynow_dialog_error_text")).getText());
+									}
+
+								}
+
+							} else {
+
+								listpos += 2;
+
+							}
+
+						} catch (NoSuchElementException e) {
+
+							System.out.println("No vino el listado");
+							Thread.sleep(2000);
+
+						}
+
+					}
+
+				}
+
+			}
+
+		}
+
+	}
+
+}
